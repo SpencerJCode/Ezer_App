4593e194becffc7dafe765ab113cd4044673f442
Evan Wiorek <124269000+EvanWiorek@users.noreply.github.com>
2023-05-22 17:36:14 -0500

    added models

---
+++ Ezer_App/Shared/LoginUser.cs
@@ -0,0 +1,18 @@
+#pragma warning disable CS8618
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+namespace Ezer_App.Shared;
+
+public class LoginUser
+{
+  [Required]
+  [EmailAddress]
+  public string LogEmail { get; set; }
+  [Required]
+  [DataType(DataType.Password)]
+  public string LogPassword { get; set; }
+}
---
+++ Ezer_App/Shared/User.cs
@@ -0,0 +1,43 @@
+#pragma warning disable CS8618
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace Ezer_App.Shared;
+
+public class User
+{
+  [Key]
+  public int UserId { get; set; }
+  [Required]
+  public string FirstName { get; set; }
+  [Required]
+  public string LastName { get; set; }
+  [Required]
+  public string Email { get; set; }
+  [Required]
+  public string Password { get; set; }
+  public bool IsMidwife { get; set; } = false;
+  public bool IsDoula { get; set; } = false;
+  public DateTime DueDate { get;set; }
+  public string PhoneNumber { get;set; }
+  public string AddressStreet { get;set; }
+  public string AddressState { get;set; }
+  public string AddressCity { get;set; }
+  public string AddressZipcode { get;set; }
+  public string EmergencyFirstName { get;set; }
+  public string EmergencyLastName { get;set; }
+  public string EmergencyNumber { get;set; }
+  public string SpouseFirstName { get;set; }
+  public string SpouseLastName { get;set; }
+  public string SpouseNumber { get;set; }
+  public DateTime CreatedAt { get; set; } = DateTime.Now;
+  public DateTime UpdatedAt { get; set; } = DateTime.Now;
+
+  [NotMapped]
+  [Requireds]
+  public string ConfirmPassword { get; set; }
+}
